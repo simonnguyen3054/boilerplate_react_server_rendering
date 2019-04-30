a115b17f768aa1355033b57c84f6231653cceea6
Simon Nguyen <simonnguyen3054@gmail.com>
2019-04-29 18:30:08 -0700

    add helper function

--- server/src/index.js
+++ server/src/index.js
@@ -1,25 +1,13 @@
 import express from "express";
-import React from "react";
-import { renderToString } from "react-dom/server";
-import App from "./client/App";
 
 const app = express();
+import renderer from "./helpers/renderer";
 
 //making public repo static
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  const content = renderToString(<App/>);
-  const html = `
-    <html>
-      <head></head>
-      <body>
-        <div id="root">${content}</div>
-        <script src="bundle.js"></script>
-      </body>
-    </html>`
-
-  res.send(html);
+  res.send(renderer());
 });
 
 app.listen(3000, () => {
